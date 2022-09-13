f2077863d79719ff3d52cedda8f0e6b49137a328
oleghmelka <oleghmelka@gmail.com>
2022-09-14 01:17:56 +0300

    added news component and added some more functionality to cryptocurrencies component

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '5647fff6b7msh79ee4f470998bffp1e0174jsnb52c21bda27e',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+})
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi
